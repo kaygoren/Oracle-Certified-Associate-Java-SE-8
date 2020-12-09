e323ceac4dddefd7a9b369466da46e4a3dc49762
kenan.aygoren <kenan.aygoren@intra.local>
2020-12-09 17:24:51 +0300

    edge case variable names and diff between variable types

---
+++ variables/_C.java
@@ -0,0 +1,30 @@
+public class _C {
+
+    // the name must begin with a letter or $ or _
+    // subsequent characters may also be number.
+
+    private static int $; // is ok but int _ is not ok.
+    
+    private static boolean bool; // class variable, default value is false
+    private float f; // instance variable, default is 0.0
+
+
+    public String __s;
+    protected long L = 1L;
+
+    public void eat(int piecesOfCheese) {  // this method consists TWO LOCAL VARIABLES.
+        int bitesOfCheese = 1; 
+    }
+
+    public static void main(String[] args) {
+
+        _C _c = new _C();
+
+        String a_b;  // local variable, need to be assigned to use.
+
+        System.out.println(_c.__s); // null
+        System.out.println($); // 0
+        // System.out.print(a_b); // compilation error, local variables require assignment before referencing them.
+    
+    }
+}
