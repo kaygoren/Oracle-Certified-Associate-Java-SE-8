a2711b18e7f985a8160e5c80dc52bc72204cdc17
kenan.aygoren <kenan.aygoren@intra.local>
2020-12-11 17:19:07 +0300

    array and arraylist examples added

--- ArrayLists.java
+++ ArrayLists.java
@@ -1,23 +1,11 @@
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-public class ArraysAndLists {
+public class ArrayLists {
     
-    public static void main(String[] args) {
+    public static void main(String args[]) {
 
-        final String names[] = new String[3];
-        names[1] = "kaygoren";  // names = [null, "kaygoren", null]
-
-        // different initialization of arrays
-        final int[] nums1 = new int[]{1,2};
-        final int nums2[] = {1,2,3};
-        final int[][] nums3 = {{1,2}, {1}, {3,4}};
-
-        for(int[] nums : nums3) {
-            for(int i=0; i<nums.length; i++) {
-                System.out.print(nums[i] + " ");
-            }
-            System.out.println();
-        }
         
         // different initialization of lists
         final List<Integer> numbers1 = new ArrayList<Integer>(Arrays.asList(1,2,3,4));
@@ -30,8 +18,7 @@ public class ArraysAndLists {
         numbers3.set(2,300);
         numbers4.set(3,400);
 
-        System.out.println(names.length);  // for array
-        System.out.println(names[1].length()); // for string
+        
         System.out.println(numbers3.size()); // for list
 
         // lists can be printed as it is but arrays cannot!
@@ -39,5 +26,13 @@ public class ArraysAndLists {
         System.out.println(numbers2); // [1, 200, 3]
         System.out.println(numbers3); // [1, 2, 300]
         System.out.println(numbers4); // [100, 2, 3, 400]
+
+        ArrayList list1 = new ArrayList(); // pre Java5 way of creating ArrayList, still legal.
+        list1.add(5);
+        list1.add("kenan");
+        System.out.println(list1);
+
+        Float f = Float.valueOf("1.0");
+
     }
 }
---
+++ Arrays.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Arrays {
+    
+    public static void main(String[] args) {
+
+        final String names[] = new String[3];
+        names[1] = "kaygoren";  // names = [null, "kaygoren", null]
+
+        System.out.println(names.length);  // for array
+        System.out.println(names[1].length()); // for string
+
+        // different initialization of arrays
+        final int[] nums1 = new int[]{1,2,8,4,0};
+        final int nums2[] = {1,2,3};
+        final int[][] nums3 = {{1,2}, {1}, {3,4}, {0,6,3}};
+        final int[][] nums4 = new int[3][];
+        nums4[0] = new int[2];
+        nums4[1] = new int[4];
+        // nums4[2] = {6,4,2};  // not allowed
+
+        for(int i=0; i<nums4.length; i++) {
+            if(nums4[i] != null)   // nums4[2] points to null
+                for(int j=0; j<nums4[i].length; j++) 
+                    System.out.print(nums4[i][j] + " ");    
+            System.out.println();
+        }
+
+        Arrays.sort(nums1);
+        // Arrays.sort(nums3);  // runtime error
+
+        for(int[] nums : nums3) {
+            for(int i=0; i<nums.length; i++) {
+                System.out.print(nums[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
