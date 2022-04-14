032e0204c2ee1cb24ffa12d06204567ec3f10a43
Kenan Aygören <kenanaygoren@gmail.com>
2022-04-14 11:54:48 +0300

    update

--- Kenan.java
+++ Kenan.java
@@ -1,11 +1,11 @@
-class Emel {
+class Ahmet {
     public static final void main(String args[]) {
         int a=5, b=4;
         while(a == b);
         for(; a<3; a++);
         StringBuilder s = new StringBuilder("ken");
         s.append(s);
-        System.out.print("emel");
+        System.out.print("ahmet");
         System.out.print(s);
         System.out.print(A.a);
     }
